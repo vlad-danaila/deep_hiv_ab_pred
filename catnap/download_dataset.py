3956922d3738d0d4b54dc2e73cac5a5e3c471b1c
Vlad Danaila <vlad.danaila@yahoo.com>
2021-10-16 16:35:02 +0300

    automate catnap download

--- catnap/download_dataset.py
+++ catnap/download_dataset.py
@@ -1,14 +1,6 @@
 from util.tools import create_folder, download_file
 from constants import *
 
-create_folder(CATNAP_FOLDER)
-download_file(ASSAY_DOWNLOAD, ASSAY_FILE)
-download_file(ANTIBODIES_HEAVY_DOWNLOAD, ANTIBODIES_HEAVY_FILE)
-download_file(ANTIBODIES_LIGHT_DOWNLOAD, ANTIBODIES_LIGHT_FILE)
-download_file(VIRUS_DOWNLOAD, VIRUS_FILE)
-download_file(VIRUS_WITH_PNGS_DONLOAD, VIRUS_WITH_PNGS_FILE)
-download_file(ANTIBODIES_DETAILS_DOWNLOAD, ANTIBODIES_DETAILS_FILE)
-
 """ The virus sequences are aligned, sometimes the characters # and * are inserted by alignment software. 
 Unfortunately, we are unsure about the exact meaning of these characters, and they could be software specific. 
 For this reason, we are removing them. """
@@ -20,6 +12,16 @@ def removeHashes(path):
     with open(path, 'w') as file:
         file.write(content)
 
-removeHashes(VIRUS_FILE)
+def download_catnap():
+    create_folder(CATNAP_FOLDER)
+    download_file(ASSAY_DOWNLOAD, ASSAY_FILE)
+    download_file(ANTIBODIES_HEAVY_DOWNLOAD, ANTIBODIES_HEAVY_FILE)
+    download_file(ANTIBODIES_LIGHT_DOWNLOAD, ANTIBODIES_LIGHT_FILE)
+    download_file(VIRUS_DOWNLOAD, VIRUS_FILE)
+    download_file(VIRUS_WITH_PNGS_DONLOAD, VIRUS_WITH_PNGS_FILE)
+    download_file(ANTIBODIES_DETAILS_DOWNLOAD, ANTIBODIES_DETAILS_FILE)
+    removeHashes(VIRUS_FILE)
 
+if __name__ == '__main__':
+    download_catnap()
 
