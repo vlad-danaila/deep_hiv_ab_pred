1b14e33371afda78a5fe88b81782984ed96a2933
Vlad Rares Danaila <vlad.danaila@ro.ibm.com>
2021-11-05 20:36:58 +0200

    log auc

--- util/metrics.py
+++ util/metrics.py
@@ -11,13 +11,18 @@ def log_metrics_per_cv_antibody(cv_metrics, antibody):
     cv_std_acc = cv_metrics[:, ACCURACY].std()
     cv_mean_mcc = cv_metrics[:, MATTHEWS_CORRELATION_COEFFICIENT].mean()
     cv_std_mcc = cv_metrics[:, MATTHEWS_CORRELATION_COEFFICIENT].std()
+    cv_mean_auc = cv_metrics[:, AUC].mean()
+    cv_std_auc = cv_metrics[:, AUC].std()
     print('CV Mean Acc', cv_mean_acc, 'CV Std Acc', cv_std_acc)
     print('CV Mean MCC', cv_mean_mcc, 'CV Std MCC', cv_std_mcc)
+    print('CV Mean AUC', cv_mean_auc, 'CV Std AUC', cv_std_auc)
     mlflow.log_metrics({
         f'cv mean acc {antibody}': cv_mean_acc,
         f'cv std acc {antibody}': cv_std_acc,
         f'cv mean mcc {antibody}': cv_mean_mcc,
-        f'cv std mcc {antibody}': cv_std_mcc
+        f'cv std mcc {antibody}': cv_std_mcc,
+        f'cv mean auc {antibody}': cv_mean_auc,
+        f'cv std auc {antibody}': cv_std_auc
     })
     return cv_mean_acc, cv_mean_mcc
 
