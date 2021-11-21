852fad94c5a4a5706e14dcdaf17ba45746c400c7
Vlad Rares Danaila <vlad.danaila@ro.ibm.com>
2021-11-21 13:54:49 +0200

    added hyperparam for amino props only

--- global_constants.py
+++ global_constants.py
@@ -9,3 +9,4 @@ HYPERPARAM_FOLDER = join('deep_hiv_ab_pred', 'hyperparameters')
 
 DEFAULT_CONF = join(HYPERPARAM_FOLDER, 'hyperparam_iceri_v2_uniform_prune_treshold_05_trial_330.json')
 # DEFAULT_CONF = join(HYPERPARAM_FOLDER, 'hyperparam_iceri_v2_hold_out_prune_treshold_01_trial_162.json')
+# DEFAULT_CONF = join(HYPERPARAM_FOLDER, 'hyperparam_gru_gru_props_onlu_trial_388.json')
