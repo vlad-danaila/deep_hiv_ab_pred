cafececa32a2681338596a0310c0d438b5bfa6a3
Vlad Rares Danaila <vlad.danaila@ro.ibm.com>
2021-11-24 19:02:49 +0200

    paratome output report analyze

--- catnap/analyze_paratome_output.py
+++ catnap/analyze_paratome_output.py
@@ -1,47 +1,36 @@
-from os.path import join
 from deep_hiv_ab_pred.catnap.constants import AB_LIGHT_CDR, AB_HEAVY_CDR
-from statistics import mean, median
 
-def extract_cdr_indexes(text):
-    text_between_parantheses = text[ text.find('(') + 1 : text.find(')') ]
-    begin, end = tuple(text_between_parantheses.split('-'))
-    return int(begin), int(end)
+SEQ_PARSE = 'SEQ_PARSE'
+CDR_PARSE = 'CDR_PARSE'
 
-def gather_cdr_indexes(cdr_file):
-    cdr_1_begin, cdr_2_begin, cdr_3_begin, cdr_1_end, cdr_2_end, cdr_3_end  = [], [], [], [], [], [],
-    with open(cdr_file) as file:
-        for line in file:
-            if not line.startswith('ABR'):
-                continue
-            begin, end = extract_cdr_indexes(line)
-            if line.startswith('ABR L1') or line.startswith('ABR H1'):
-                cdr_1_begin.append(begin)
-                cdr_1_end.append(end)
-            elif line.startswith('ABR L2') or line.startswith('ABR H2'):
-                cdr_2_begin.append(begin)
-                cdr_2_end.append(end)
-            elif line.startswith('ABR L3')  or line.startswith('ABR H3'):
-                cdr_3_begin.append(begin)
-                cdr_3_end.append(end)
-    return cdr_1_begin, cdr_2_begin, cdr_3_begin, cdr_1_end, cdr_2_end, cdr_3_end
+def extract_cdr_data(line):
+    cdr_num, cdr_seq, indexes = tuple(line.split(' '))
+    cdr_num = int(cdr_num[3]) # It has the form "ABR1:" "ABR2:" "ABR3:"
+    indexes = indexes[1:-2] # remove parantheses and new line
+    indexes = tuple((int(s) for s in indexes.split('-')))
+    return cdr_num, cdr_seq, indexes
 
 def parse_paratome_report(report_file):
     cdrs_in_seq = {}
-    seq_parse = False
-    cdr_parse = False
-    seq = ''
+    state = None
     with open(report_file) as file:
         for line in file:
             if line.startswith('>paratome'):
-                seq_parse = True
+                if state == CDR_PARSE:
+                    cdrs_in_seq[seq] = cdrs
+                cdrs = []
+                seq = ''
+                state = SEQ_PARSE
                 continue
-            if seq_parse:
-                seq += line[:-1]
+            if state == SEQ_PARSE:
                 if line == '\n':
-                    seq_parse = False
-                    cdr_parse = True
-            if cdr_parse:
-                #TODO
+                    state = CDR_PARSE
+                else:
+                    seq += line[:-1]
+                continue
+            if state == CDR_PARSE:
+                cdrs.append(extract_cdr_data(line))
+    return cdrs_in_seq
 
 if __name__ == '__main__':
-    parse_paratome_report(AB_LIGHT_CDR)
+    cdrs_by_seq = parse_paratome_report(AB_LIGHT_CDR)
