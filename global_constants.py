204456b6c341dc08c24c8bb5b2a331207f0a8b0a
Vlad Rares Danaila <vlad.danaila@ro.ibm.com>
2021-11-29 22:59:47 +0200

    hyperparam conf for optimized one hot and prpos

--- global_constants.py
+++ global_constants.py
@@ -9,5 +9,6 @@ HYPERPARAM_FOLDER = join('deep_hiv_ab_pred', 'hyperparameters')
 
 DEFAULT_CONF = join(HYPERPARAM_FOLDER, 'hyperparam_fc_att_gru_uniform_one_hot_1_layer_trial_159.json')
 # DEFAULT_CONF = join(HYPERPARAM_FOLDER, 'hyperparam_fc_att_gru_uniform_props_only_1_layer_trial_252.json')
+# DEFAULT_CONF = join(HYPERPARAM_FOLDER, 'hyperparam_fc_att_gru_uniform_one_hot_and_props_1_layer_trial_213.json')
 # DEFAULT_CONF = join(HYPERPARAM_FOLDER, 'hyperparam_iceri_v2_uniform_prune_treshold_05_trial_330.json')
 # DEFAULT_CONF = join(HYPERPARAM_FOLDER, 'hyperparam_iceri_v2_hold_out_prune_treshold_01_trial_162.json')
