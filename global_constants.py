d62360e50f6cc327b6eaf3969e2654def770dbc5
Vlad Rares Danaila <vlad.danaila@ro.ibm.com>
2022-01-05 12:39:44 +0200

    ading new hiperparam config found with multivariae tpe without prunner

--- global_constants.py
+++ global_constants.py
@@ -7,8 +7,9 @@ EMBEDDING = 'PROPS-ONLY'
 
 HYPERPARAM_FOLDER = join('deep_hiv_ab_pred', 'hyperparameters')
 
+DEFAULT_CONF = join(HYPERPARAM_FOLDER, 'hyperparam_fc_att_gru_uniform_props_only_1_layer_multivariate_tpe_no_prunner_trial_319.json')
 # DEFAULT_CONF = join(HYPERPARAM_FOLDER, 'hyperparam_fc_att_gru_uniform_one_hot_1_layer_trial_159.json')
-DEFAULT_CONF = join(HYPERPARAM_FOLDER, 'hyperparam_fc_att_gru_uniform_props_only_1_layer_trial_252.json')
+# DEFAULT_CONF = join(HYPERPARAM_FOLDER, 'hyperparam_fc_att_gru_uniform_props_only_1_layer_trial_252.json')
 # DEFAULT_CONF = join(HYPERPARAM_FOLDER, 'hyperparam_fc_att_gru_uniform_one_hot_and_props_1_layer_trial_213.json')
 # DEFAULT_CONF = join(HYPERPARAM_FOLDER, 'hyperparam_iceri_v2_uniform_prune_treshold_05_trial_330.json')
 # DEFAULT_CONF = join(HYPERPARAM_FOLDER, 'hyperparam_iceri_v2_hold_out_prune_treshold_01_trial_162.json')
