bedbac8fdb48ae2836389b3ab4fe1f2ed4f3441b
Vlad Rares Danaila <vlad.danaila@ro.ibm.com>
2022-01-05 18:01:18 +0200

    work in progress

--- preprocessing/pytorch_dataset.py
+++ preprocessing/pytorch_dataset.py
@@ -5,13 +5,14 @@ from deep_hiv_ab_pred.preprocessing.aminoacids import amino_to_index
 
 class AssayDataset(t.utils.data.Dataset):
 
-    def __init__(self, assays, antibody_light_seq, antibody_heavy_seq, virus_seq, pngs_mask_to_kemr_tensor):
+    def __init__(self, assays, antibody_light_seq, antibody_heavy_seq, virus_seq, pngs_mask_to_kemr_tensor, ab_to_types):
         super().__init__()
         self.assays = assays
         self.antibody_light_seq = antibody_light_seq
         self.antibody_heavy_seq = antibody_heavy_seq
         self.virus_seq = virus_seq
         self.pngs_mask_to_kemr_tensor = pngs_mask_to_kemr_tensor
+        self.ab_to_types = ab_to_types
 
     def __getitem__(self, i):
         id, antibody, virus, ground_truth = self.assays[i]
@@ -19,7 +20,8 @@ class AssayDataset(t.utils.data.Dataset):
         antibody_heavy_tensor = self.antibody_heavy_seq[antibody]
         virus_tensor          = self.virus_seq[virus]
         pngs_mask_tensor      = self.pngs_mask_to_kemr_tensor[virus]
-        return antibody_light_tensor, antibody_heavy_tensor, virus_tensor, pngs_mask_tensor, ground_truth
+        ab_types_ground_truth = self.ab_to_types[antibody]
+        return antibody_light_tensor, antibody_heavy_tensor, virus_tensor, pngs_mask_tensor, ground_truth, ab_types_ground_truth
 
     def __len__(self):
         return len(self.assays)
@@ -33,6 +35,7 @@ def zero_padding(batch):
     virus        = [b[2] for b in batch]
     pngs_mask    = [b[3] for b in batch]
     ground_truth = [b[4] for b in batch]
+    ab_types_ground_truth = [b[5] for b in batch]
     batched_ab_light = t.stack(ab_light)
     batched_ab_heavy = t.stack(ab_heavy)
     batched_virus = t.nn.utils.rnn.pad_sequence(virus, batch_first=True, padding_value = amino_to_index['X'])
@@ -40,5 +43,6 @@ def zero_padding(batch):
     batched_pngs_mask = t.nn.utils.rnn.pad_sequence(pngs_mask, batch_first=True, padding_value = 0)
     #batched_pngs_mask = t.stack(pngs_mask)
     batched_ground_truth = t.tensor(ground_truth, dtype=t.float32, device=device)
+    batched_ab_types_ground_truth = t.tensor(ab_types_ground_truth, dtype=t.float32, device=device)
     # print(batched_ab_light.shape, batched_ab_heavy.shape, batched_virus.shape, batched_ground_truth.shape)
-    return batched_ab_light, batched_ab_heavy, batched_virus, batched_pngs_mask, batched_ground_truth
+    return batched_ab_light, batched_ab_heavy, batched_virus, batched_pngs_mask, batched_ground_truth, batched_ab_types_ground_truth
