06cae46b727c1c15691fe8fe2d22a3cfda557b40
Vlad Rares Danaila <vlad.danaila@ro.ibm.com>
2022-01-20 18:22:06 +0200

    added hyperparam optimized config

--- global_constants.py
+++ global_constants.py
@@ -7,8 +7,9 @@ EMBEDDING = 'PROPS-ONLY'
 
 HYPERPARAM_FOLDER = join('deep_hiv_ab_pred', 'hyperparameters')
 
+DEFAULT_CONF = join(HYPERPARAM_FOLDER, 'hyperparam_fc_gru_ab_clsf_unfirm_props_only_1_layer_trial_408.json')
 # DEFAULT_CONF = join(HYPERPARAM_FOLDER, 'hyperparam_fc_att_gru_uniform_one_hot_1_layer_trial_159.json')
-DEFAULT_CONF = join(HYPERPARAM_FOLDER, 'hyperparam_fc_att_gru_uniform_props_only_1_layer_trial_252.json')
+# DEFAULT_CONF = join(HYPERPARAM_FOLDER, 'hyperparam_fc_att_gru_uniform_props_only_1_layer_trial_252.json')
 # DEFAULT_CONF = join(HYPERPARAM_FOLDER, 'hyperparam_fc_att_gru_uniform_one_hot_and_props_1_layer_trial_213.json')
 # DEFAULT_CONF = join(HYPERPARAM_FOLDER, 'hyperparam_iceri_v2_uniform_prune_treshold_05_trial_330.json')
 # DEFAULT_CONF = join(HYPERPARAM_FOLDER, 'hyperparam_iceri_v2_hold_out_prune_treshold_01_trial_162.json')
