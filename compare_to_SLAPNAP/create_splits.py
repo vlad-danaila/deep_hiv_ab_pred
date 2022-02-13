62625ea50e28d6034eb30794468087a21213e231
Vlad Rares Danaila <vlad.danaila@ro.ibm.com>
2022-02-13 23:27:20 +0200

    fix

--- compare_to_SLAPNAP/create_splits.py
+++ compare_to_SLAPNAP/create_splits.py
@@ -13,15 +13,14 @@ def get_slpnap_ab_virus_pairs():
         if not ab_results_file.endswith('json'):
             continue
         ab_results = read_json_file(join(SLAPNAP_RESULTS, ab_results_file))
-        antibody = ab_results_file.split('.')[0]
+        antibody = '.'.join(ab_results_file.split('.')[:-1])
         ab_viruses[antibody] = ab_results['virus_ids']
     return ab_viruses
 
 def create_splits_to_compare_with_slapnap(catnap):
-    rawi_data = read_json_file(RAWI_DATA)
+    slapnap_data = get_slpnap_ab_virus_pairs()
     splits = {}
-    for antibody, viruses in rawi_data.items():
-        virus_ids = [v.split('.')[-2] for v in viruses]
+    for antibody, virus_ids in slapnap_data.items():
         pretrain_data = [ data[0] for data in catnap if data[1] != antibody ]
         cv_tuples = [(antibody, virus) for virus in virus_ids]
         cv_data = [data[0] for data in catnap if (data[1], data[2]) in cv_tuples]
@@ -30,8 +29,8 @@ def create_splits_to_compare_with_slapnap(catnap):
             print('Skipping', antibody)
             continue
         print(antibody)
-        cv_splits = cross_validation_splits(cv_data, ground_truths)
-        splits[antibody] = { 'pretraining': pretrain_data, 'cross_validation': cv_splits }
+        # cv_splits = cross_validation_splits(cv_data, ground_truths)
+        splits[antibody] = { 'pretraining': pretrain_data, 'cross_validation': [] }
     return splits
 
 if __name__ == '__main__':
