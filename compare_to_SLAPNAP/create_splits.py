295d989dded3faf943d7053807d0a42c4c90b5e2
Vlad Rares Danaila <vlad.danaila@ro.ibm.com>
2022-02-14 21:01:38 +0200

    data split to compare with SLAPNAP

--- compare_to_SLAPNAP/create_splits.py
+++ compare_to_SLAPNAP/create_splits.py
@@ -15,6 +15,11 @@ def get_slpnap_ab_data():
         ab_viruses[antibody] = (ab_results['virus_ids'], ab_results['folds'])
     return ab_viruses
 
+def map_to_ids(antibody, viruses, catnap):
+    cv_data = [data[0] for data in catnap if data[1] == antibody and data[2] in viruses]
+    assert len(cv_data) == len(viruses)
+    return cv_data
+
 # TODO use catnap data ids instead of viruses
 def cross_validation_splits(virus_ids, folds, antibody, catnap):
     virus_ids_np = np.array(virus_ids)
@@ -26,8 +31,9 @@ def cross_validation_splits(virus_ids, folds, antibody, catnap):
         train_splits_indexes = [i for i in range(len(virus_ids_np)) if i not in indexes]
         train_viruses = virus_ids_np[train_splits_indexes]
         assert len(train_viruses) + len(test_viruses) == len(virus_ids)
-
-        cv_splits.append({ 'train': train_viruses, 'test': test_viruses })
+        train_ids = map_to_ids(antibody, train_viruses, catnap)
+        test_ids = map_to_ids(antibody, test_viruses, catnap)
+        cv_splits.append({ 'train': train_ids, 'test': test_ids })
     return cv_splits
 
 def create_splits_to_compare_with_slapnap(catnap):
@@ -35,12 +41,6 @@ def create_splits_to_compare_with_slapnap(catnap):
     splits = {}
     for antibody, (virus_ids, folds) in slapnap_data.items():
         pretrain_data = [ data[0] for data in catnap if data[1] != antibody ]
-        cv_tuples = [(antibody, virus) for virus in virus_ids]
-        cv_data = [data[0] for data in catnap if (data[1], data[2]) in cv_tuples]
-        ground_truths = [data[3] for data in catnap if (data[1], data[2]) in cv_tuples]
-        if len(cv_data) != len(cv_tuples):
-            print('Skipping', antibody)
-            continue
         print(antibody)
         cv_splits = cross_validation_splits(virus_ids, folds, antibody, catnap)
         splits[antibody] = { 'pretraining': pretrain_data, 'cross_validation': cv_splits }
